1e9d99ea7283361082d6d6f7a60a16309efce034
yashug <gosulayaswanth2@gmail.com>
2023-11-27 23:15:37 +0530

    adds revalidate to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@/components/ui/separator";
 import PostList from "@/components/posts/post-list";
 import { fetchTopPosts } from "@/db/queries/posts";
 
-export const revalidate = 10
+export const revalidate = 10;
 
 export default async function Home() {
   return (
@@ -15,7 +15,7 @@ export default async function Home() {
       </div>
       <div className="border shadow py-3 px-2 min-w-fit">
         <TopicCreateForm />
-        <Separator className="my-2"/>
+        <Separator className="my-2" />
         <h3 className="text-lg font-semibold mb-4">Topics</h3>
         <TopicsList />
       </div>
